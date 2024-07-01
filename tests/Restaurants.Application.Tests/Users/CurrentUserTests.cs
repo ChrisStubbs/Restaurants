20e5265317cbdd0527b7ec4ef684e71b0bb33f63
Chris Stubbs <christopher.stubbs@transparity.com>
2024-07-01 17:54:16 +0100

    add and fix tests folders

--- tests/Restaurants.Application.Tests/Users/CurrentUserTests.cs
+++ tests/Restaurants.Application.Tests/Users/CurrentUserTests.cs
@@ -1,35 +1,60 @@
 ﻿using FluentAssertions;
-using Restaurants.Application.Users;
+using Restaurants.Domain.Constants;
 using Xunit;
 
-namespace Restaurants.Application.Tests.Users;
+namespace Restaurants.Application.Users.Tests;
 
 public class CurrentUserTests
 {
-    //TestMethod_Scenario_ExpectedResult
-    //[Fact()]
-    //public void IsInRole_WithMatchingRole_ShouldReturnTrue()
-    //{
-    //    //Arrange
-    //    var currentUser = new CurrentUser("1", "test@test.com", ["Admin", "User"]);
+    // TestMethod_Scenario_ExpectResult
+    [Theory()]
+    [InlineData(UserRoles.Admin)]
+    [InlineData(UserRoles.User)]
+    public void IsInRole_WithMatchingRole_ShouldReturnTrue(string roleName)
+    {
+        // arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User], null, null);
 
-    //    //Act
-    //    var isInRole = currentUser.IsInRole("Admin");
-    //    //Assert
+        // act
 
-    //    isInRole.Should().BeTrue();
-    //}
+        var isInRole = currentUser.IsInRole(roleName);
 
-    //[Fact()]
-    //public void IsInRole_WithNoMatchingRole_ShouldReturnFalse()
-    //{
-    //    //Arrange
-    //    var currentUser = new CurrentUser("1", "test@test.com", ["Admin", "User"]);
+        // assert
 
-    //    //Act
-    //    var isInRole = currentUser.IsInRole("Guest");
-    //    //Assert
+        isInRole.Should().BeTrue();
 
-    //    isInRole.Should().BeFalse();
-    //}
+    }
+
+
+    [Fact()]
+    public void IsInRole_WithNoMatchingRole_ShouldReturnFalse()
+    {
+        // arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // act
+
+        var isInRole = currentUser.IsInRole(UserRoles.Owner);
+
+        // assert
+
+        isInRole.Should().BeFalse();
+
+    }
+
+    [Fact()]
+    public void IsInRole_WithNoMatchingRoleCase_ShouldReturnFalse()
+    {
+        // arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // act
+
+        var isInRole = currentUser.IsInRole(UserRoles.Admin.ToLower());
+
+        // assert
+
+        isInRole.Should().BeFalse();
+
+    }
 }
